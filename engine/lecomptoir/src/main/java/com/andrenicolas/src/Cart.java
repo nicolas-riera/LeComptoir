3401939287041e0ce25671484b4c5d7af07f8b00
andrebtw <gwandrewgd@gmail.com>
2026-09-21 15:58:10 +0200

    add: Cart and Checkout classed added.

---
+++ engine/lecomptoir/src/main/java/com/andrenicolas/src/Cart.java
@@ -0,0 +1,35 @@
+package com.andrenicolas.src;
+import java.util.List;
+
+// all the lines and the subtotal calculation
+public class Cart {
+    private List<CartLine>  cartlines;
+    private double          subTotal;
+
+    public void AddCartlines(List<CartLine> cartlines){
+        for (CartLine cartline : cartlines){
+            this.AddCartline(cartline);
+        }
+    }
+
+    public void AddCartline(CartLine cartline){
+        cartlines.add(cartline);
+    }
+
+    private void calcSubTotal(){
+        this.subTotal = 0;
+        for (CartLine cartline : this.cartlines){
+            this.subTotal = this.subTotal + cartline.getSubtotal();
+        }
+    }
+
+    public double getSubTotal(){
+        this.calcSubTotal();
+        return this.subTotal;
+    }
+
+    public List<CartLine> getCartlines(){
+        return this.cartlines;
+    }
+
+}
---
+++ engine/lecomptoir/src/main/java/com/andrenicolas/src/Checkout.java
@@ -0,0 +1,21 @@
+package com.andrenicolas.src;
+
+
+// entry point that produces the receipt
+public class Checkout {
+    public String showReceipt(Cart cart){
+        StringBuilder receipt = new StringBuilder();
+
+        receipt.append("--------RECEIPT-------\n");
+        for (CartLine cartline : cart.getCartlines()){
+              receipt.append(String.format("%s [%s] x%d : %.2f €\n",
+              cartline.getProduct().getLabel(),
+              cartline.getProduct().getCategory(),
+              cartline.getQuantity(),
+              cartline.getSubtotal()));
+        }
+        receipt.append("------------------------\n");
+        receipt.append(String.format("TOTAL : %.2f €\n", cart.getSubTotal()));
+        return receipt.toString();
+    }
+}
