866a966d188674fcd0c486e111aebbdff0523216
nicolas-riera <nicolas.riera@laplateforme.io>
2026-09-21 15:19:47 +0200

    other: emptied app.java

--- engine/lecomptoir/src/main/java/com/andrenicolas/App.java
+++ engine/lecomptoir/src/main/java/com/andrenicolas/App.java
@@ -1,13 +1,9 @@
 package com.andrenicolas;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
     }
 }
