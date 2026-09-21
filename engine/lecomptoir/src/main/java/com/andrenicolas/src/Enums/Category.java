b9203d212d313c034cc59e20979ffd42f3c5c46b
nicolas-riera <nicolas.riera@laplateforme.io>
2026-09-21 15:32:26 +0200

    add: added product, category enum and cartline

---
+++ engine/lecomptoir/src/main/java/com/andrenicolas/src/Enums/Category.java
@@ -0,0 +1,12 @@
+package com.andrenicolas.src.Enums;
+
+public enum Category {
+    DRINKS,
+    FRESH_FOOD,
+    GROCERY,
+    FROZEN,
+    BAKERY,
+    HYGIENE_BEAUTY,
+    HOME_CARE,
+    OTHER
+}
