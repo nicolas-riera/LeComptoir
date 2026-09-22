fd7a780ea6eee9d5f6d5b9e3bacf583d275d3f28
nicolas-riera <nicolas.riera@laplateforme.io>
2026-09-22 09:56:44 +0200

    add(wip): http server init
    Co-authored-by: André <andrebtw@users.noreply.github.com>

--- engine/lecomptoir/src/main/java/com/andrenicolas/App.java
+++ engine/lecomptoir/src/main/java/com/andrenicolas/App.java
@@ -1,9 +1,17 @@
 package com.andrenicolas;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
 
+        server.setExecutor(null);
+        server.start();
+        System.out.println("Server started on http://localhost:8080");
     }
 }
